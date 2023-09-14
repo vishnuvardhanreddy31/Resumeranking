8bc2fc7b8ac40d261015df397e07494396fb9f82
Goli Vishnuvardhan Reddy <104411008+vishnuvardhanreddy31@users.noreply.github.com>
2023-09-14 14:06:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 
-#loading models
-clf = pickle.load(open('clf.pkl','rb'))
-tfidfd = pickle.load(open('tfidf.pkl','rb'))
+# Loading models
+clf = pickle.load(open('clf.pkl', 'rb'))
+tfidfd = pickle.load(open('tfidf.pkl', 'rb'))
 
+# Text cleaning function
 def clean_resume(resume_text):
     clean_text = re.sub('http\S+\s*', ' ', resume_text)
     clean_text = re.sub('RT|cc', ' ', clean_text)
@@ -19,10 +20,14 @@ def clean_resume(resume_text):
     clean_text = re.sub(r'[^\x00-\x7f]', r' ', clean_text)
     clean_text = re.sub('\s+', ' ', clean_text)
     return clean_text
-# web app
+
+# Streamlit UI
 def main():
-    st.title("NLP based Resume Ranking for Job Matching")
-    uploaded_file = st.file_uploader('Upload Resume', type=['txt','pdf'])
+    st.title("Resume to Job Category Prediction")
+    st.sidebar.title("Options")
+
+    # File upload widget
+    uploaded_file = st.sidebar.file_uploader("Upload a resume (in TXT or PDF format)", type=['txt', 'pdf'])
 
     if uploaded_file is not None:
         try:
@@ -35,43 +40,44 @@ def main():
         cleaned_resume = clean_resume(resume_text)
         input_features = tfidfd.transform([cleaned_resume])
         prediction_id = clf.predict(input_features)[0]
-        st.write(prediction_id)
 
         # Map category ID to category name
         category_mapping = {
-            15: "Java Developer",
-            23: "Testing",
+            0: "Advocate",
+            1: "Arts",
+            2: "Automation Testing",
+            3: "Blockchain",
+            4: "Business Analyst",
+            5: "Civil Engineer",
+            6: "Data Science",
+            7: "Database",
             8: "DevOps Engineer",
-            20: "Python Developer",
-            24: "Web Designing",
+            9: "DotNet Developer",
+            10: "ETL Developer",
+            11: "Electrical Engineering",
             12: "HR",
             13: "Hadoop",
-            3: "Blockchain",
-            10: "ETL Developer",
-            18: "Operations Manager",
-            6: "Data Science",
-            22: "Sales",
-            16: "Mechanical Engineer",
-            1: "Arts",
-            7: "Database",
-            11: "Electrical Engineering",
             14: "Health and fitness",
-            19: "PMO",
-            4: "Business Analyst",
-            9: "DotNet Developer",
-            2: "Automation Testing",
+            15: "Java Developer",
+            16: "Mechanical Engineer",
             17: "Network Security Engineer",
+            18: "Operations Manager",
+            19: "PMO",
+            20: "Python Developer",
             21: "SAP Developer",
-            5: "Civil Engineer",
-            0: "Advocate",
+            22: "Sales",
+            23: "Testing",
+            24: "Web Designing",
         }
 
         category_name = category_mapping.get(prediction_id, "Unknown")
 
-        st.write("Predicted Category:", category_name)
+        # Display results
+        st.header("Resume Analysis Results")
+        st.subheader("Predicted Job Category:")
+        st.success(category_name)
 
-
-
-# python main
+# Python main
 if __name__ == "__main__":
     main()
+    
